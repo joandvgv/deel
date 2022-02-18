741df42ab1d24be32d7d3e7a8ed71abd2302230f
Joan <joandv.gil@gmail.com>
2022-02-18 20:25:45 -0300

    chore: clean up

--- src/routes/jobs.router.ts
+++ src/routes/jobs.router.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { Op } from "sequelize";
 import { getProfile } from "../middleware/getProfile";
 import Contract, { ContractStatus } from "../models/Contract";
 import Job from "../models/Job";
@@ -27,19 +26,6 @@ router.get("/jobs/unpaid", async (req, res) => {
   const jobs = contracts?.flatMap(({ jobs: item }) => item) ?? [];
 
   res.json({ jobs });
-});
-
-router.get("/contracts", async (req, res) => {
-  const clientId = req.profile.id;
-
-  const contracts = await Contract.findAll({
-    where: {
-      ClientId: clientId,
-      status: { [Op.not]: ContractStatus.terminated },
-    },
-  });
-
-  res.json({ contracts });
 });
 
 export default router;
